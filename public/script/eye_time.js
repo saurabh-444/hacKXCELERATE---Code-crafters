680c1273b0c972a5976cbc92abbfd336e0ed7bbe
saurabh-444 <149707402+saurabh-444@users.noreply.github.com>
2023-11-03 11:15:03 +0530

    Add files via upload

---
+++ public/script/eye_time.js
@@ -0,0 +1,25 @@
+
+const timeLeftValue = document.querySelector(".time-left");
+let timeLeft = 0;
+
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+const runTimeLeftUpdate = async () => {
+  const alarm = await browser.alarms.get("Breaks");
+  if (alarm) {
+    console.log("hello");
+    timeLeft = alarm.scheduledTime - Date.now();
+  }
+  if (timeLeft - 1000 >= 0) {
+    timeLeftValue.textContent = millisToMinutesAndSeconds(timeLeft);
+  } else {
+    timeLeftValue.textContent = "disabled"
+  }
+  setTimeout(runTimeLeftUpdate, 1000);
+};
+
+runTimeLeftUpdate();
